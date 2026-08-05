8c9d595c1699f67948c857e02b2b209cf0ce858d
leandrocalfin <leitto.c92@gmail.com>
2026-08-05 16:41:14 -0300

    arreglos servicio

--- backend/src/routes/servicioRoutes.js
+++ backend/src/routes/servicioRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { obtenerServicios, crearServicio, actualizarServicio, eliminarServicio } from '../controllers/servicioController.js';
 import { verificarToken } from '../middlewares/authMiddleware.js';
-import { uploadFotos as upload } from '../config/cloudinary.js'; // O la ruta donde tengas tu configuración de multer/cloudinary
+import { uploadFotos as upload } from '../config/cloudinary.js';
 
 const router = Router();
 
 router.get('/', obtenerServicios);
-router.post('/', verificarToken, upload.single('imagen'), crearServicio);
-router.put('/:id', verificarToken, upload.single('imagen'), actualizarServicio);
+router.post('/', verificarToken, upload.any(), crearServicio); // 👈 Usamos any() para evitar el choque de nombres
+router.put('/:id', verificarToken, upload.any(), actualizarServicio);
 router.delete('/:id', verificarToken, eliminarServicio);
 
 export default router;
